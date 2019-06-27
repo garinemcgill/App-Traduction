b7a52ef58e8a7ee5599400bd1b75e21bae396167
Garine Imamedjian <garine.imamedjian@mail.mcgill.ca>
2019-06-27 15:25:03 -0400

    fixed create to go 2 ways for language, convo, person, survey

--- App-Traduction-Backend/src/main/java/cirque/apptraduction/service/AppTraductionService.java
+++ App-Traduction-Backend/src/main/java/cirque/apptraduction/service/AppTraductionService.java
@@ -3,7 +3,9 @@ package cirque.apptraduction.service;
 import java.sql.Date;
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,6 +33,12 @@ public class AppTraductionService {
 	TraductionAppManagerRepository managerRepository;
 	
 	
+	//*************************************************************************************************************//
+	//**********************************************CREATE METHODS*************************************************//
+	//*************************************************************************************************************//
+	
+	
+	/*Creates manager*/
 	@Transactional
 	public TraductionAppManager createTraductionAppManager() {
 		TraductionAppManager manager = new TraductionAppManager();
@@ -38,18 +46,39 @@ public class AppTraductionService {
 		return manager;
 	}
 
+	
+	/*Creates language
+	 * 		adds language to list in manager */
 	@Transactional
 	public Language createLanguage(String name, TraductionAppManager manager) {
 		if(name == null || name == "") {
 			throw new IllegalArgumentException("The name of a language cannot be null or empty");
 		}
+		
+		Set<Language> newLanguages = manager.getLanguage();
+		if(newLanguages == null) {
+			newLanguages = new HashSet<Language>();
+		}
+		for (Language iLanguage : newLanguages) {
+			if (iLanguage.getName().equals(name))
+				throw new IllegalArgumentException("This language already exists.");
+		}
+		
 		Language language = new Language();
 		language.setName(name);
 		language.setTraductionAppManager(manager);
 		languageRepository.save(language);
+		
+		newLanguages.add(language);
+		manager.setLanguage(newLanguages);
+		managerRepository.save(manager);
+		
 		return language;
 	}
 	
+	
+	/*Creates conversation
+	 * 		adds conversation to list in manager */
 	@Transactional
 	public Conversation createConversation(Date date, Time time, Boolean withGoogle, TraductionAppManager manager) {
 		if(date == null || time == null) {
@@ -61,9 +90,21 @@ public class AppTraductionService {
 		conversation.setWithGoogle(withGoogle);
 		conversation.setTraductionAppManager(manager);
 		conversationRepository.save(conversation);
+		
+		Set<Conversation> newConversations = manager.getConversation();
+		if(newConversations == null) {
+			newConversations = new HashSet<Conversation>();
+		}
+		newConversations.add(conversation);
+		manager.setConversation(newConversations);
+		managerRepository.save(manager);
+		
 		return conversation;
 	}
 	
+	
+	/*Creates Person 
+	 * 		adds person to list in languages and list in conversation */
 	@Transactional
 	public Person createPerson(String department, Conversation conversation, Language language) {
 		if(department == null || department == "") {
@@ -77,9 +118,29 @@ public class AppTraductionService {
 		person.setConversation(conversation);
 		person.setLanguage(language);
 		personRepository.save(person);
+		
+		Set<Person> newPersonsConversation = conversation.getPerson();
+		if(newPersonsConversation == null) {
+			newPersonsConversation = new HashSet<Person>();
+		}
+		newPersonsConversation.add(person);
+		conversation.setPerson(newPersonsConversation);
+		conversationRepository.save(conversation);
+		
+		Set<Person> newPersonsLanguage = language.getPerson();
+		if(newPersonsLanguage == null) {
+			newPersonsLanguage = new HashSet<Person>();
+		}
+		newPersonsLanguage.add(person);
+		language.setPerson(newPersonsLanguage);
+		languageRepository.save(language);
+		
 		return person;
 	}
 	
+	
+	/* Creates survey
+	 * 		adds survey to person */
 	@Transactional
 	public Survey createSurvey(int helpsWork, int replacesService, int rating, Person person) {
 		Survey survey = new Survey();
@@ -88,6 +149,10 @@ public class AppTraductionService {
 		survey.setRating(rating);
 		survey.setPerson(person);
 		surveyRepository.save(survey);
+		
+		person.setSurvey(survey);
+		personRepository.save(person);
+		
 		return survey;
 	}
 	
@@ -110,5 +175,12 @@ public class AppTraductionService {
 		textRepository.save(text);
 		return text;
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
