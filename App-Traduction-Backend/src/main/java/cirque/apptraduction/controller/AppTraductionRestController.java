ef2559888d3edd6105ac2a565f00fb274d2caf4f
Garine Imamedjian <garine.imamedjian@mail.mcgill.ca>
2019-06-28 14:30:18 -0400

    started post mappings

--- App-Traduction-Backend/src/main/java/cirque/apptraduction/controller/AppTraductionRestController.java
+++ App-Traduction-Backend/src/main/java/cirque/apptraduction/controller/AppTraductionRestController.java
@@ -1,10 +1,42 @@
 package cirque.apptraduction.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import cirque.apptraduction.dto.*;
+import cirque.apptraduction.Model.*;
+import cirque.apptraduction.service.*;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class AppTraductionRestController {
+	
+	@Autowired
+	AppTraductionService service;
 
+	
+	/*
+	@PostMapping(value = { "/newConversation", "/newConversation/" })
+	public ConversationDto createConversation() throws IllegalArgumentException {
+		
+		TraductionAppManager manager = service.getManager();
+		if (manager == null) {
+			manager = service.createTraductionAppManager();
+		}
+		
+		
+		
+		Conversation conversation = service.createConversation(manager);
+		return convertToDto(conversation);
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	
 }
