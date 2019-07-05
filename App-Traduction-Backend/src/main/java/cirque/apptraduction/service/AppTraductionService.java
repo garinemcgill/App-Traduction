2d1ac160b6561828694ad29ef19643c9b2d712fb
Garine Imamedjian <garine.imamedjian@mail.mcgill.ca>
2019-07-05 13:00:39 -0400

    google imports recognized, speech to text from mic input and local file added

--- App-Traduction-Backend/src/main/java/cirque/apptraduction/service/AppTraductionService.java
+++ App-Traduction-Backend/src/main/java/cirque/apptraduction/service/AppTraductionService.java
@@ -16,6 +16,11 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.DataLine.Info;
 import javax.sound.sampled.TargetDataLine;
 
+import com.google.api.gax.rpc.ApiStreamObserver;
+import com.google.api.gax.rpc.BidiStreamingCallable;
+import com.google.api.gax.rpc.ClientStream;
+import com.google.api.gax.rpc.ResponseObserver;
+import com.google.api.gax.rpc.StreamController;
 import com.google.cloud.speech.v1.RecognitionAudio;
 import com.google.cloud.speech.v1.RecognitionConfig;
 import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
@@ -23,7 +28,14 @@ import com.google.cloud.speech.v1.RecognizeResponse;
 import com.google.cloud.speech.v1.SpeechClient;
 import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
 import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.cloud.speech.v1.StreamingRecognitionConfig;
+import com.google.cloud.speech.v1.StreamingRecognitionResult;
+import com.google.cloud.speech.v1.StreamingRecognizeRequest;
+import com.google.cloud.speech.v1.StreamingRecognizeResponse;
+import com.google.common.util.concurrent.SettableFuture;
 import com.google.protobuf.ByteString;
+
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -475,6 +487,95 @@ public class AppTraductionService {
 	//*************************************************************************************************************//
 	//********************************************GOOGLE API CALLS*********************************************//
 	//*************************************************************************************************************//
+	
+	
+	/**
+	 * Performs streaming speech recognition on raw PCM audio data.
+	 *
+	 * @param fileName the path to a PCM audio file to transcribe.
+	 */
+	public static void streamingRecognizeFile(String fileName) throws Exception, IOException {
+	  Path path = Paths.get(fileName);
+	  byte[] data = Files.readAllBytes(path);
+
+	  // Instantiates a client with GOOGLE_APPLICATION_CREDENTIALS
+	  try (SpeechClient speech = SpeechClient.create()) {
+
+	    // Configure request with local raw PCM audio
+	    RecognitionConfig recConfig =
+	        RecognitionConfig.newBuilder()
+	            .setEncoding(AudioEncoding.LINEAR16)
+	            .setLanguageCode("en-US")
+	            .setSampleRateHertz(16000)
+	            .setModel("default")
+	            .build();
+	    StreamingRecognitionConfig config =
+	        StreamingRecognitionConfig.newBuilder().setConfig(recConfig).build();
+
+	    class ResponseApiStreamingObserver<T> implements ApiStreamObserver<T> {
+	      private final SettableFuture<List<T>> future = SettableFuture.create();
+	      private final List<T> messages = new java.util.ArrayList<T>();
+
+	      @Override
+	      public void onNext(T message) {
+	        messages.add(message);
+	      }
+
+	      @Override
+	      public void onError(Throwable t) {
+	        future.setException(t);
+	      }
+
+	      @Override
+	      public void onCompleted() {
+	        future.set(messages);
+	      }
+
+	      // Returns the SettableFuture object to get received messages / exceptions.
+	      public SettableFuture<List<T>> future() {
+	        return future;
+	      }
+	    }
+
+	    ResponseApiStreamingObserver<StreamingRecognizeResponse> responseObserver =
+	        new ResponseApiStreamingObserver<>();
+
+	    BidiStreamingCallable<StreamingRecognizeRequest, StreamingRecognizeResponse> callable =
+	        speech.streamingRecognizeCallable();
+
+	    @SuppressWarnings("deprecation")
+		ApiStreamObserver<StreamingRecognizeRequest> requestObserver =
+	        callable.bidiStreamingCall(responseObserver);
+
+	    // The first request must **only** contain the audio configuration:
+	    requestObserver.onNext(
+	        StreamingRecognizeRequest.newBuilder().setStreamingConfig(config).build());
+
+	    // Subsequent requests must **only** contain the audio data.
+	    requestObserver.onNext(
+	        StreamingRecognizeRequest.newBuilder()
+	            .setAudioContent(ByteString.copyFrom(data))
+	            .build());
+
+	    // Mark transmission as completed after sending the data.
+	    requestObserver.onCompleted();
+
+	    List<StreamingRecognizeResponse> responses = responseObserver.future().get();
+
+	    for (StreamingRecognizeResponse response : responses) {
+	      // For streaming recognize, the results list has one is_final result (if available) followed
+	      // by a number of in-progress results (if iterim_results is true) for subsequent utterances.
+	      // Just print the first result here.
+	      StreamingRecognitionResult result = response.getResultsList().get(0);
+	      // There can be several alternative transcripts for a given chunk of speech. Just use the
+	      // first (most likely) one here.
+	      SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+	      System.out.printf("Transcript : %s\n", alternative.getTranscript());
+	    }
+	  }
+	}
+	
+	
 	
 	
 	/** Performs microphone streaming speech recognition with a duration of 1 minute. */
